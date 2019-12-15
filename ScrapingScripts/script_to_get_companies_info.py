8aad69479ec1fa23f59b39ab1b39e884afed1d2f
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2019-12-15 18:36:49 +0530

    added try except

--- ScrapingScripts/script_to_get_companies_info.py
+++ ScrapingScripts/script_to_get_companies_info.py
@@ -29,27 +29,30 @@ class GetCompaniesInfo(object):
 		company_dict_list = []
 		com_count = 0
 		for com_id in self._com_ids:
-			print("Scraping com_id: {0} ...............".format(com_id))
-			page = requests.get(self.get_request_url(com_id))
-			soup = BeautifulSoup(page.text, "html.parser")
+			try:
+				print("Scraping com_id: {0} ...............".format(com_id))
+				page = requests.get(self.get_request_url(com_id))
+				soup = BeautifulSoup(page.text, "html.parser")
 
-			table = soup.select('table')[0]
-			data_raw = table.find_all('td')
-			del data_raw[0], data_raw[0], data_raw[4], data_raw[4]
+				table = soup.select('table')[0]
+				data_raw = table.find_all('td')
+				del data_raw[0], data_raw[0], data_raw[4], data_raw[4]
 
-			company_dict = {}
-			for i in range(int(len(data_raw)/2)):
-				company_dict[data_raw[i*2].string] = self.remove_special_chars(data_raw[i*2+1].string)
+				company_dict = {}
+				for i in range(int(len(data_raw)/2)):
+					company_dict[data_raw[i*2].string] = self.remove_special_chars(data_raw[i*2+1].string)
 
-			old_keys = list(company_dict.keys())
+				old_keys = list(company_dict.keys())
 
-			for key in old_keys:
-				company_dict[self._key_mapping[key]] = company_dict[key]
-				del company_dict[key]
-			company_dict['com_id'] = com_id
-			company_dict_list.append(company_dict)
-			com_count += 1
-			print("Scraped com_id: {0};\t {1} out of {2} Done! ".format(com_id, com_count, len(self._com_ids)))
+				for key in old_keys:
+					company_dict[self._key_mapping[key]] = company_dict[key]
+					del company_dict[key]
+				company_dict['com_id'] = com_id
+				company_dict_list.append(company_dict)
+				com_count += 1
+				print("Scraped com_id: {0};\t {1} out of {2} Done! ".format(com_id, com_count, len(self._com_ids)))
+			except Exception as e:
+				print(e)
 		return company_dict_list
 
 	def get_request_url(self, com_id):
--- ScrapingScripts/script_to_get_companies_profile_info.py
+++ ScrapingScripts/script_to_get_companies_profile_info.py
@@ -36,46 +36,49 @@ class GetCompaniesProfileInfo(object):
 		company_dict_list = []
 		com_count = 0
 		for ids in self._com_ids:
-			com_id, jnf_id = ids[0], ids[1]
-			print("Scraping com_id: {0}, jnf_id: {1} ...............".format(com_id, jnf_id))
-			page = requests.get(self.get_request_url(com_id, jnf_id))
-			soup = BeautifulSoup(page.text, "html.parser")
+			try:
+				com_id, jnf_id = ids[0], ids[1]
+				print("Scraping com_id: {0}, jnf_id: {1} ...............".format(com_id, jnf_id))
+				page = requests.get(self.get_request_url(com_id, jnf_id))
+				soup = BeautifulSoup(page.text, "html.parser")
 
-			table = soup.select('table')[0]
-			data_raw = table.find_all('td')
-			del data_raw[0:4], data_raw[6], data_raw[7]
+				table = soup.select('table')[0]
+				data_raw = table.find_all('td')
+				del data_raw[0:4], data_raw[6], data_raw[7]
 
-			company_dict = {
-				'com_id': com_id,
-				'jnf_id': jnf_id,
-				'type': data_raw[0].string,
-				'profile': data_raw[1].string,
-				'ctc': data_raw[2].string,
-				'contract': data_raw[3].string,
-				'criteria': data_raw[4].string,
-				'cgpa_cutoff': data_raw[5].string,
-				'about': data_raw[6].string,
-				'selection': data_raw[7].string
-			}
+				company_dict = {
+					'com_id': com_id,
+					'jnf_id': jnf_id,
+					'type': data_raw[0].string,
+					'profile': data_raw[1].string,
+					'ctc': data_raw[2].string,
+					'contract': data_raw[3].string,
+					'criteria': data_raw[4].string,
+					'cgpa_cutoff': data_raw[5].string,
+					'about': data_raw[6].string,
+					'selection': data_raw[7].string
+				}
 
-			del data_raw[0:8]
+				del data_raw[0:8]
 
-			dep_str = ''
-			first = True
-			for tag in data_raw:
-				try:
-					if "background-color: lightgrey" in tag['style']:
-						if first:
-							dep_str += (self._dep_mapping[self.remove_special_chars(tag.string)])
-							first = False
-						else:
-							dep_str += ('_' + self._dep_mapping[self.remove_special_chars(tag.string)])
-				except:
-					pass
-			company_dict['dep_str'] = dep_str
-			company_dict_list.append(company_dict)
-			com_count += 1
-			print("Scraped com_id: {0}, jnf_id: {1};\t {2} out of {3} Done! ".format(com_id, jnf_id, com_count, len(self._com_ids)))
+				dep_str = ''
+				first = True
+				for tag in data_raw:
+					try:
+						if "background-color: lightgrey" in tag['style']:
+							if first:
+								dep_str += (self._dep_mapping[self.remove_special_chars(tag.string)])
+								first = False
+							else:
+								dep_str += ('_' + self._dep_mapping[self.remove_special_chars(tag.string)])
+					except:
+						pass
+				company_dict['dep_str'] = dep_str
+				company_dict_list.append(company_dict)
+				com_count += 1
+				print("Scraped com_id: {0}, jnf_id: {1};\t {2} out of {3} Done! ".format(com_id, jnf_id, com_count, len(self._com_ids)))
+			except Exception as e:
+				print(e)
 		return company_dict_list
 
 	def get_request_url(self, com_id, jnf_id):
