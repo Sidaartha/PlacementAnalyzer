2cd20baca0b1542f31bb1720dd8b18c998f5450e
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2019-12-15 20:19:09 +0530

    conditional statement to ignore non placement data

--- ScrapingScripts/script_to_get_companies_profile_info.py
+++ ScrapingScripts/script_to_get_companies_profile_info.py
@@ -46,37 +46,40 @@ class GetCompaniesProfileInfo(object):
 				data_raw = table.find_all('td')
 				del data_raw[0:4], data_raw[6], data_raw[7]
 
-				company_dict = {
-					'com_id': com_id,
-					'jnf_id': jnf_id,
-					'type': data_raw[0].string,
-					'profile': data_raw[1].string,
-					'ctc': data_raw[2].string,
-					'contract': data_raw[3].string,
-					'criteria': data_raw[4].string,
-					'cgpa_cutoff': data_raw[5].string,
-					'about': data_raw[6].string,
-					'selection': data_raw[7].string
-				}
+				if data_raw[0].string == 'PLACEMENT':
+					company_dict = {
+						'com_id': com_id,
+						'jnf_id': jnf_id,
+						'type': data_raw[0].string,
+						'profile': data_raw[1].string,
+						'ctc': data_raw[2].string,
+						'contract': data_raw[3].string,
+						'criteria': data_raw[4].string,
+						'cgpa_cutoff': data_raw[5].string,
+						'about': data_raw[6].string,
+						'selection': data_raw[7].string
+					}
 
-				del data_raw[0:8]
+					del data_raw[0:8]
 
-				dep_str = ''
-				first = True
-				for tag in data_raw:
-					try:
-						if "background-color: lightgrey" in tag['style']:
-							if first:
-								dep_str += (self._dep_mapping[self.remove_special_chars(tag.string)])
-								first = False
-							else:
-								dep_str += ('_' + self._dep_mapping[self.remove_special_chars(tag.string)])
-					except:
-						pass
-				company_dict['dep_str'] = dep_str
-				company_dict_list.append(company_dict)
-				com_count += 1
-				print("Scraped com_id: {0}, jnf_id: {1};\t {2} out of {3} Done! ".format(com_id, jnf_id, com_count, len(self._com_ids)))
+					dep_str = ''
+					first = True
+					for tag in data_raw:
+						try:
+							if "background-color: lightgrey" in tag['style']:
+								if first:
+									dep_str += (self._dep_mapping[self.remove_special_chars(tag.string)])
+									first = False
+								else:
+									dep_str += ('_' + self._dep_mapping[self.remove_special_chars(tag.string)])
+						except:
+							pass
+					company_dict['dep_str'] = dep_str
+					company_dict_list.append(company_dict)
+					com_count += 1
+					print("Scraped com_id: {0}, jnf_id: {1};\t {2} out of {3} Done! ".format(com_id, jnf_id, com_count, len(self._com_ids)))
+				else:
+					print("Ignoned com_id: {0}, jnf_id: {1} as {2};".format(com_id, jnf_id, data_raw[0].string))
 			except Exception as e:
 				print(e)
 		return company_dict_list
