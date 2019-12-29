f8d1e9c40e729cb04f158963ff959cc51c435325
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2019-12-29 09:59:14 +0530

    aded students json

--- ScrapingScripts/script_to_get_json_objects.py
+++ ScrapingScripts/script_to_get_json_objects.py
@@ -14,14 +14,16 @@ def csv_to_dict(file_path):
 			object_list.append(object_dict)
 	return object_list
 
-file_path = ['OutputFiles/placements.csv', 'OutputFiles/placements_companies.csv', 'OutputFiles/placements_companies_final.csv', 'OutputFiles/placements_companies_profile_final.csv']
+file_path = ['OutputFiles/placements.csv', 'OutputFiles/placements_companies.csv', 'OutputFiles/placements_companies_final.csv', 'OutputFiles/placements_companies_profile_final.csv', 'Files/students.csv']
 
 companies = csv_to_dict(file_path[2])
 companies_profile = csv_to_dict(file_path[3])
 placements_students = csv_to_dict(file_path[0])
 placements_companies = csv_to_dict(file_path[1])
+students_data = csv_to_dict(file_path[4])
 
 companies_list = list(set([comp['com_id'] for comp in placements_companies]))
+students_list = list(set([stud['roll_no'] for stud in students_data]))
 
 companies_dict = {}
 for com_id in companies_list:
@@ -60,3 +62,15 @@ for com_id in companies_list:
 
 with open('../DataJSON/profiles.json', 'w', encoding='utf-8') as f:
     json.dump(profile_dict, f, ensure_ascii=False, indent=4)
+
+students_dict = {}
+for roll in students_list:
+	for stud in students_data:
+		if  stud['roll_no'] == roll:
+			del stud['roll_no']
+			students_dict[roll] = stud
+			students_data.remove(stud)
+			break
+
+with open('../DataJSON/students.json', 'w', encoding='utf-8') as f:
+    json.dump(students_dict, f, ensure_ascii=False, indent=4)
