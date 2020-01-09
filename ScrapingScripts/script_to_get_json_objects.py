f8605b90b8cfdd25320cfb6ca0c486f5f246b1dc
Sidaartha Reddy <sidaarthareddy1998@gmail.com>
2020-01-09 19:21:12 +0530

    get dep wise profiles

--- ScrapingScripts/script_to_get_json_objects.py
+++ ScrapingScripts/script_to_get_json_objects.py
@@ -21,7 +21,46 @@ companies_profile = csv_to_dict(file_path[3])
 placements_students = csv_to_dict(file_path[0])
 placements_companies = csv_to_dict(file_path[1])
 students_data = csv_to_dict(file_path[4])
+departments = {
+	"AE": "AEROSPACE ENGINEERING",
+	"AG": "AGRICULTURAL AND FOOD ENGINEERING",
+	"AR": "ARCHITECTURE AND REGIONAL PLANNING",
+	"BT": "BIOTECHNOLOGY",
+	"CH": "CHEMICAL ENGINEERING",
+	"CY": "CHEMISTRY",
+	"CE": "CIVIL ENGINEERING",
+	"CS": "COMPUTER SCIENCE AND ENGINEERING",
+	"EE": "ELECTRICAL ENGINEERING",
+	"EC": "ELECTRONICS AND ELECTRICAL COMMUNICATION ENGG.",
+	"GG": "GEOLOGY AND GEOPHYSICS",
+	"HS": "HUMANITIES AND SOCIAL SCIENCES",
+	"IM": "INDUSTRIAL AND SYSTEMS ENGINEERING",
+	"MA": "MATHEMATICS",
+	"ME": "MECHANICAL ENGINEERING",
+	"MT": "METALLURGICAL AND MATERIALS ENGINEERING",
+	"MI": "MINING ENGINEERING",
+	"NA": "OCEAN ENGG AND NAVAL ARCHITECTURE",
+	"PH": "PHYSICS",
+	"IP": "RAJIV GANDHI SCHOOL OF INTELLECTUAL PROPERTY LAW",
+	"AT": "ADVANCED TECHNOLOGY DEVELOPMENT CENTRE",
+	"BS": "BIO SCIENCE",
+	"ET": "CENTRE FOR EDUCATIONAL TECHNOLOGY",
+	"CL": "CENTRE FOR OCEANS, RIVERS, ATMOSPHERE AND LAND SCIENCES (CORAL)",
+	"CR": "CRYOGENIC ENGINEERING",
+	"ES": "ENERGY SCIENCE AND ENGINEERING",
+	"EV": "ENVIRONMENTAL SCIENCE AND ENGINEERING",
+	"TE": "G.S SANYAL SCHOOL OF TELECOMMUNICATION",
+	"MS": "MATERIALS SCIENCE CENTRE",
+	"NS": "NANO SCIENCE AND TECHNOLOGY",
+	"EP": "RAJENDRA MISHRA SCHOOL OF ENGG ENTREPRENEURSHIP",
+	"ID": "RANBIR AND CHITRA GUPTA SCHOOL OF INFRASTRUCTURE DESIGN AND MNGT.",
+	"RT": "RUBBER TECHNOLOGY",
+	"MM": "SCHOOL OF MEDICAL SCIENCE AND TECHNOLOGY",
+	"WM": "SCHOOL OF WATER RESOURCES",
+	"RE": "SUBIR CHOWDHURY SCHOOL OF QUALITY AND RELIABILITY"
+}
 
+departments_list = list(departments.keys())
 companies_list = list(set([comp['com_id'] for comp in placements_companies]))
 students_list = list(set([stud['roll_no'] for stud in students_data]))
 
@@ -47,8 +86,8 @@ for com_id in companies_list:
 					companies_dict[com_id]['students'] = students
 			break
 
-with open('../DataJSON/companies.json', 'w', encoding='utf-8') as f:
-    json.dump(companies_dict, f, ensure_ascii=False, indent=4)
+# with open('../DataJSON/companies.json', 'w', encoding='utf-8') as f:
+#     json.dump(companies_dict, f, ensure_ascii=False, indent=4)
 
 profile_dict = {}
 for com_id in companies_list:
@@ -60,8 +99,8 @@ for com_id in companies_list:
 			profiles[profile['jnf_id']] = profile
 	profile_dict[com_id] = profiles
 
-with open('../DataJSON/profiles.json', 'w', encoding='utf-8') as f:
-    json.dump(profile_dict, f, ensure_ascii=False, indent=4)
+# with open('../DataJSON/profiles.json', 'w', encoding='utf-8') as f:
+#     json.dump(profile_dict, f, ensure_ascii=False, indent=4)
 
 students_dict = {}
 for roll in students_list:
@@ -72,5 +111,25 @@ for roll in students_list:
 			students_data.remove(stud)
 			break
 
-with open('../DataJSON/students.json', 'w', encoding='utf-8') as f:
-    json.dump(students_dict, f, ensure_ascii=False, indent=4)
+# with open('../DataJSON/students.json', 'w', encoding='utf-8') as f:
+#     json.dump(students_dict, f, ensure_ascii=False, indent=4)
+
+departments_dict = {}
+for dep in departments_list:
+	departments_dict[dep] = {
+		'com_ids': []
+	}
+for profile in companies_profile:
+	dep_list = profile['dep_str'].split('_')
+	com_id = profile['com_id']
+	jnf_id = profile['jnf_id']
+	for dep in dep_list:
+		if dep != "":
+			if com_id in departments_dict[dep]['com_ids']:
+				departments_dict[dep][com_id].append(jnf_id)
+			else:
+				departments_dict[dep]['com_ids'].append(com_id)
+				departments_dict[dep][com_id] = [jnf_id]
+
+# with open('../DataJSON/departments.json', 'w') as f:
+#     json.dump(departments_dict, f, ensure_ascii=False, indent=4)
